aedfb8a5e6a839890d72d530ee5c557b04f826b3
start.vaadin.com <no-reply@vaadin.com>
2021-06-14 18:03:10 +0000

    Generated project

---
+++ src/main/java/com/example/memorymanagement/views/login/LoginView.java
@@ -0,0 +1,30 @@
+package com.example.memorymanagement.views.login;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.example.memorymanagement.views.main.MainView;
+
+@Route(value = "", layout = MainView.class)
+@PageTitle("Login")
+public class LoginView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public LoginView() {
+        addClassName("login-view");
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        add(name, sayHello);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+    }
+
+}
