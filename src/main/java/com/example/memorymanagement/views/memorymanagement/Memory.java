669f6ff9c80c721ddc72affd8d1be901e4559959
Denilson Anachury <denilsoncastro1997@gmail.com>
2021-06-15 15:14:55 -0500

    Use memory of device to Build select

--- src/main/java/com/example/memorymanagement/views/memorymanagement/Memory.java
+++ src/main/java/com/example/memorymanagement/views/memorymanagement/Memory.java
@@ -2,8 +2,18 @@ package com.example.memorymanagement.views.memorymanagement;
 
 public final class Memory {
 
-    public static final short MAX_SIZE = 8;
+    public static final int MB = 1024 * 1024 * 1024;
+    public static final short MIN_SIZE = 2;
+    public static final short DEFAULT_SIZE = 8;
+    public static final short MAX_SIZE = memorySize();
 
     private Memory() {
+    }
+
+    private static short memorySize() {
+        final var usableMemory = Math.round((double) Runtime.getRuntime().maxMemory() / MB);
+        if (usableMemory < MIN_SIZE)
+            return DEFAULT_SIZE;
+        return (short) usableMemory;
     }
 }
